9a1d791797325134b549239ab0e6b2ff0130560d
YS739 <suk921@gmail.com>
2022-12-11 19:07:25 +0900

    working app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-function App() {
+const App = () => {
+  // Component state(이 안에서만 씀)
+  const [tempBalance, setTempBalance] = useState(0);
+
+  // store 상에 있는 state를 읽어오는 부분
+  const balance = useSelector((state) => state.account.balance);
+  //  dispatch를 사용하기 위해 변수화 시킨 것
+  const dispatch = useDispatch();
+
+  // input을 제어하는 함수
+  const onBalanceChangeHandler = (event) => {
+    const { value } = event.target;
+    // 앞에 '+'를 붙여 number로 만들어 줌
+    setTempBalance(+value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "100px" }}>
+      <hr />
+      <br />
+      현재 잔액은? {balance}달러
+      <br />
+      <input type="number" onChange={onBalanceChangeHandler}></input>
+      <br />
+      <button>입금</button>
+      <button>출금</button>
     </div>
   );
-}
+};
 
 export default App;
