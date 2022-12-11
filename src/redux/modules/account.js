14397dbad0d592e16cd08c48e6ce0d0b3b64e92e
YS739 <suk921@gmail.com>
2022-12-11 18:46:15 +0900

    made modules account.js

--- src/redux/modules/account.js
+++ src/redux/modules/account.js
@@ -1,6 +1,18 @@
 // Action Value
-
 const DEPOSIT_DOLLAR = "DEPOSIT_DOLLAR";
 const WITHDRAW_DOLLAR = "WITHDRAW_DOLLAR";
 
-// Action creator
+// Action Creator
+export const depositDollar = (payload) => {
+  return {
+    type: DEPOSIT_DOLLAR,
+    payload,
+  };
+};
+
+export const withdrawDollar = (payload) => {
+  return {
+    type: WITHDRAW_DOLLAR,
+    payload,
+  };
+};
