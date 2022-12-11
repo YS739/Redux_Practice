36814bd7a83ed56138758fac1725e59126517df7
YS739 <suk921@gmail.com>
2022-12-11 18:52:36 +0900

    made reducer

--- src/redux/modules/account.js
+++ src/redux/modules/account.js
@@ -16,3 +16,30 @@ export const withdrawDollar = (payload) => {
     payload,
   };
 };
+
+const initialState = {
+  balance: 0,
+};
+
+// Reducer
+const account = (state = { initialState }, action) => {
+  switch (action.type) {
+    //  입금 : payload로 받아온 값을 더함
+    case DEPOSIT_DOLLAR: {
+      return {
+        balance: state.balance + action.payload,
+      };
+    }
+
+    //  출금 : payload로 받아온 값을 뺌
+    case WITHDRAW_DOLLAR: {
+      return {
+        balance: state.balance - action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default account;
